ccaaedd67bb8d8ad578e6e24bc2160439251f3fb
jaesjeon <jaesjeon@student.42seoul.kr>
2022-07-21 11:23:04 +0900

    Makefile update, mutex test example

--- philo/philo.c
+++ philo/philo.c
@@ -6,26 +6,41 @@
 /*   By: jaesjeon <jaesjeon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 22:02:42 by jaesjeon          #+#    #+#             */
-/*   Updated: 2022/07/20 22:17:39 by jaesjeon         ###   ########.fr       */
+/*   Updated: 2022/07/21 11:16:42 by jaesjeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./philo.h"
 
-void	print_hello(int *i)
+void	print_hello(t_mutex_test *lock_section)
 {
-	printf("hello! %d\n", *i);
+	pthread_mutex_lock(&lock_section->mutex);
+	printf("hello! %ld\n", lock_section->counter);
+	lock_section->counter++;
+	pthread_mutex_unlock(&lock_section->mutex);
 }
 
 int	main(void)
 {
-	pthread_t	thread;
-	int	i = 5;
+	pthread_t		thread[5];
+	void			*exit_status[5];
+	t_mutex_test	lock_section;
+	int				i;
 
-	while (i)
+	pthread_mutex_init(&lock_section.mutex, NULL);
+	pthread_mutex_lock(&lock_section.mutex);
+	lock_section.counter = 1;
+	i = 0;
+	while (i < 5)
 	{
-		pthread_create(&thread, NULL, (void *)print_hello, &i);
+		pthread_create(&thread[i], NULL, (void *)print_hello, &lock_section);
 		usleep(100);
+		i++;
+	}
+	pthread_mutex_unlock(&lock_section.mutex);
+	while (i > 0)
+	{
+		pthread_join(thread[i - 1], &exit_status[i - 1]);
 		i--;
 	}
 	return (0);
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: jaesjeon <jaesjeon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 22:03:17 by jaesjeon          #+#    #+#             */
-/*   Updated: 2022/07/20 22:06:40 by jaesjeon         ###   ########.fr       */
+/*   Updated: 2022/07/21 11:06:50 by jaesjeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,10 @@
 # include <unistd.h>    //write, usleep
 # include <sys/time.h>  //gettimeofday
 # include <pthread.h>   //pthread function
+
+typedef struct mutex_test {
+	pthread_mutex_t	mutex;
+	size_t			counter;
+}	t_mutex_test;
 
 #endif
